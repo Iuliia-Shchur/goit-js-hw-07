ff830077dc715f4c315e3122a6526fe1f541c2a1
Iuliia-Shchur <79912506+Iuliia-Shchur@users.noreply.github.com>
2021-07-15 21:06:20 +0300

    task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,15 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+const galleryList = document.querySelector('#gallery');
+
+const addGalleryListEl = images.map(element => {
+  const addGalleryListElImg = `<li class="gallery__item"><img src="${element.url}" alt="${element.alt}" width=320 height=150></li>`;
+  
+  return addGalleryListElImg;
+});
+
+galleryList.insertAdjacentHTML("beforeend", addGalleryListEl.join(' '));
+galleryList.setAttribute("style", "list-style:none; display: flex; justify-content: space-around;");
